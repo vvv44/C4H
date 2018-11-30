4a4bafd9a03ab8f894f7d71f93407d6044fa306c
Victor Vargas <vvm1319@gmail.com>
2018-11-29 20:08:43 -0700

    managed to make 2.1.1 work, declared 2.1.2, still have to work on that function

--- Main.hs
+++ Main.hs
@@ -1,1 +1,35 @@
 module Main where
+
+--Declaration of function that will take a player as integer and return a char representation of it
+playerToChar :: Integer -> Char 
+
+--Declaration of function that will take a number from console and returns and IO value
+readSlot :: [[Int]] -> Int -> IO(Int)
+
+{-|
+Definition of playerToChar, will take the parameter, and 
+-}
+playerToChar p | (p==1) ='O'
+   | (p==0) = 'X'
+
+   
+
+--Gets a number from console
+getX = do
+     putStrLn "Enter a positive value?"
+     line <- getLine
+     let parsed = reads line :: [(Integer, String)] in
+       if length parsed < 0
+       then getX'
+       else let (x, _) = head parsed in
+         if x <= 1 
+         then do 
+            let pl = playerToChar x
+            putChar pl
+         else getX'
+     where
+       getX' = do
+         putStrLn "Invalid Input!"
+         getX
+main = do
+   getX
