7db8f19db242d20f548e6d74fe1d4cbd3447a627
Victor Vargas <vvm1319@gmail.com>
2018-12-05 13:56:30 -0700

    Added full structure of main, still have some scope errors and a problem with repromting user, have to check that when other methods from board are available

--- Main.hs
+++ Main.hs
@@ -9,13 +9,18 @@ playerToChar :: Integer -> Char
 readSlot :: [[Integer]] -> Integer -> IO(Integer)
 
 {-|
-Definition of playerToChar, will take the parameter, and return a character representationof such player
+Definition of playerToChar, will take the parameter, and return a character representation of such player
 -}
 playerToChar p | (p==2) ='O'
    | (p==1) = 'X'
 
-   
 
+--Board, player and opponent   
+bd = mkBoard 7 6
+player = mkPlayer
+opponnent = mkOpponent
+turn = 1   
+   
 --Gets a number from consoleas a slot
 readSlot bd p= do
      putStrLn "Enter a number between 1-7 or -1 to quit"
@@ -26,12 +31,12 @@ readSlot bd p= do
        else let (x, _) = head parsed in
          if x > 7  --invalid column
          then readSlot'
-			else if x < (-1)
-			then readSlot'
-			else --here we would check if number is -1 else we assume is a slot
-				if x == -1
-				then return 0 --set to terminate program
-				else return x --return the actual value read
+            else if x < (-1)
+            then readSlot'
+            else --here we would check if number is -1 else we assume is a slot
+                if x == -1
+                then return 0 --set to terminate program
+                else return x --return the actual value read
      where
        readSlot' = do
          putStrLn "Invalid Input!"
@@ -39,42 +44,37 @@ readSlot bd p= do
          
          
 main = do
---put which player's turn is it
-   let bd = mkBoard 7 6
-   let player = mkPlayer; --check if can be outside of main
-   let opponnent = mkOpponent;
-   putStrLn "It is   turn:"
-   answer <- readSlot bd player; --put our value read into answer
-   if answer!=0
-   then processMove--process turn change and repeat main
-   else 
-    return 0--terminate program
-	
-   where 
-   {-|
-	The game can end by a player winning, a board being full or the player quitting
-	We will first check if the slot is open, 
-	if so, we drop the checker, check for a player winning, or a full board
-	and change turn
-	-}
-	processTurn = do --here we will check which turn is it, process the movement and check for game ending	
-		if isSlotOpen bd answer --check if a slot is open
-		then do 
-			dropInSlot bd answer turn--missing a way to see which player's turn is it
-			if isWonBy bd turn --if player won with this movement we quit
-			then do
-				putStrLn "Congrats!!"
-				return 0 --terminate program
-			else if isFull bd --check if board is full
-			then do
-				putStrLn "It was a tie!"
-				return 0
-			else --we change the turn
-				if turn == 1
-				then let turn = 2
-				else
-					let turn = 1
-		else do 
-			putStrLn "Slot is full, select a different one."--we reprompt player to enter a different slot
-			main
-		
+    --draw the board with the methods provided by board
+    let visualBoard  = boardToStr playerToChar bd
+    putStrLn visualBoard --print a visual representation of the board
+    --put which player's turn is it
+    let plChar = playerToChar turn
+    putStrLn ("It is"  ++ plChar ++ "turn:")
+    answer <- readSlot bd player; --put our value read into answer
+    if not (answer== 0)
+    then processMove--process turn change and repeat main
+    else return 0--terminate program
+
+     {-|
+    The game can end by a player winning, a board being full or the player quitting
+    We will first check if the slot is open, 
+    if so, we drop the checker, check for a player winning, or a full board
+    and change turn
+    -}
+    where 
+        processMove = do --here we will check which turn is it, process the movement and check for game ending 
+            if isSlotOpen bd answer --check if a slot is open
+            then do 
+                if turn == 1
+                then dropInSlot bd answer player--drop it with the player
+                else dropInSlot bd answer opponnent --drop it with the opponent
+                if isWonBy bd turn --if player won with this movement we quit
+                then do
+                    putStrLn "Congrats!!" --program ends
+                else if isFull bd --check if board is full
+                then do
+                    putStrLn "It was a tie!"
+                else--we change the turn
+                    main --remmeber to change this part to change the turn
+            else putStrLn "Slot is full, select a different one." 
+            main --repromtp user
