39d191c612b30a7ec599b8abdd229ae895ead539
Felipe Leal <felipe.leal.sierra@gmail.com>
2018-12-06 11:07:37 -0700

    hello

--- Board.hs
+++ Board.hs
@@ -1,15 +1,14 @@
 module Board() where 
 
 
-
+--Makes Board
 mkboard m n = [ [ (0) | m <- [1..m] ] | n <- [1..n] ]
 
 mkPlayer = 1
 
 mkOpponent = 2
 
-randomList m = [ (0) | m <- [1..m] ] 
-
+--Counts how many columns there is 
 numSlot :: [[Int]] -> Int
 numSlot [] = 0
 numSlot (x:_) = listColumn x
@@ -18,20 +17,39 @@ listColumn :: [Int] -> Int
 listColumn [] = 0
 listColumn (x:xs) = 1 + listColumn xs
 
+--Checks if a slot is open in a certain column
+
 isSlotOpen :: [[Int]] -> Int -> Bool
 isSlotOpen [] n = False
-isSlotOpen (x:_) n = isColOpen x n  
+isSlotOpen (x:xs) n
+   | isColOpen x n
+   | otherwise = x: isSlotOpen xs n
 
 isColOpen :: [Int] -> Int -> Bool
 isColOpen x 0 = if head x == 0 then True else False
 isColOpen (x:xs) n = isColOpen xs (n-1)
 isColOpen [] _ = False
+
+--Checks if board is full
   
 isFull :: [[Int]] -> Bool
 isFull [] = False
 isFull xs =  notElem 0 (head xs)
 
+--Drops a disc in a desired slot
 
+--dropInSlot :: [[Int]] -> Integer -> Integer -> [[Int]]
+--dropInSlot x c p = replaceColumn
+
+
+
+
+
+replaceNth :: Int -> a -> [a] -> [a]
+replaceNth _ _ [] = []
+replaceNth n newVal (x:xs)
+   | n == 0 = newVal:xs
+   | otherwise = x:replaceNth (n-1) newVal xs
 
 
  
