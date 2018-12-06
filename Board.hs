369a2847bcca86ab3d8a471b2eb8a4a924740994
Victor Vargas <v.conejo@live.com>
2018-12-06 00:24:10 -0700

    Seems that conversion of board to string is not working

--- Board.hs
+++ Board.hs
@@ -2,6 +2,12 @@ module Board(
 mkBoard,
 mkPlayer,
 mkOpponent,
+numSlot,
+isSlotOpen,
+isFull,
+boardToStr,
+isWonBy,
+dropInSlot
 ) where
 
 
@@ -10,3 +16,44 @@ mkBoard m n = [ [ (0) | m <- [1..m] ] | n <- [1..n] ]
 mkPlayer = 1
 
 mkOpponent = 2
+
+randomList m = [ (0) | m <- [1..m] ] 
+
+numSlot :: [[Integer]] -> Integer
+numSlot [] = 0
+numSlot (x:_) = listColumn x
+
+listColumn :: [Integer] -> Integer
+listColumn [] = 0
+listColumn (x:xs) = 1 + listColumn xs
+
+isSlotOpen :: [[Integer]] -> Integer -> Bool
+isSlotOpen [] n = False
+isSlotOpen (x:_) n = isColOpen x n  
+
+isColOpen :: [Integer] -> Integer -> Bool
+isColOpen x 0 = if head x == 0 then True else False
+isColOpen (x:xs) n = isColOpen xs (n-1)
+isColOpen [] _ = False
+  
+isFull :: [[Integer]] -> Bool
+isFull [] = False
+isFull xs =  notElem 0 (head xs)
+
+boardToStr :: (Integer -> Char) -> [[Integer]] -> [Char]
+boardToStr playerToChar [[]] = "Empty Board" 
+boardToStr playerToChar (x:_) = makeRowString playerToChar x 
+
+makeRowString :: (Integer -> Char) -> [Integer] ->[Char]
+makeRowString playerToChar (x:xs) = " " ++ makeRowString playerToChar xs 
+makeRowString playerToChar x = if head x==0 then charToString '-' else charToString (playerToChar (head x))
+
+
+charToString :: Char -> String
+charToString c = [c]
+
+isWonBy :: [[Integer]] -> Integer -> Bool
+isWonBy [[]] 1 = False
+
+dropInSlot :: [[Integer]] -> Integer -> Integer -> [[Integer]]
+dropInSlot (_:_) x y = [[]]
--- Main.hs
+++ Main.hs
@@ -1,6 +1,6 @@
 module Main where
 import Board
-
+import System.Random
 
 --Declaration of function that will take a player as integer and return a char representation of it
 playerToChar :: Integer -> Char 
@@ -14,13 +14,15 @@ Definition of playerToChar, will take the parameter, and return a character repr
 playerToChar p | (p==2) ='O'
    | (p==1) = 'X'
 
-
 --Board, player and opponent   
 bd = mkBoard 7 6
 player = mkPlayer
 opponnent = mkOpponent
 turn = 1   
    
+
+   
+
 --Gets a number from consoleas a slot
 readSlot bd p= do
      putStrLn "Enter a number between 1-7 or -1 to quit"
@@ -42,17 +44,49 @@ readSlot bd p= do
          putStrLn "Invalid Input!"
          readSlot bd p
          
-         
-main = do
-    --draw the board with the methods provided by board
+ 
+--method to play normally
+play = do
+     --draw the board with the methods provided by board
     let visualBoard  = boardToStr playerToChar bd
     putStrLn visualBoard --print a visual representation of the board
     --put which player's turn is it
     let plChar = playerToChar turn
-    putStrLn ("It is"  ++ plChar ++ "turn:")
+    putStrLn ("It is ")
+    putChar(plChar)
+    putStrLn("'s turn:")
     answer <- readSlot bd player; --put our value read into answer
     if not (answer== 0)
-    then processMove--process turn change and repeat main
+    then --process turn change and repeat play
+        if isSlotOpen bd answer --check if a slot is open
+            then do 
+                if turn == 1
+                then do 
+                    --dropInSlot bd answer player--drop it with the player
+                    if isWonBy bd turn --if player won with this movement we quit
+                    then do
+                        putStrLn "Congrats!!" --program ends
+                        return 0
+                    else if isFull bd --check if board is full
+                    then do
+                        putStrLn "It was a tie!"
+                        return 0
+                    else--we change the turn
+                        play' --remmeber to change this part to change the turn
+                else do 
+                    --dropInSlot bd answer opponnent --drop it with the opponentif isWonBy bd turn --if player won with this movement we quit
+                    if isWonBy bd turn --if player won with this movement we quit
+                    then do
+                        putStrLn "Congrats!!" --program ends
+                        return 0
+                    else if isFull bd --check if board is full
+                    then do
+                        putStrLn "It was a tie!"
+                        return 0
+                    else--we change the turn
+                        play' --remmeber to change this part to change the turn
+            else do
+                play'
     else return 0--terminate program
 
      {-|
@@ -62,19 +96,72 @@ main = do
     and change turn
     -}
     where 
-        processMove = do --here we will check which turn is it, process the movement and check for game ending 
-            if isSlotOpen bd answer --check if a slot is open
+        play' = do
+            play
+      
+playAI = do
+     --draw the board with the methods provided by board
+    let visualBoard  = boardToStr playerToChar bd
+    putStrLn visualBoard --print a visual representation of the board
+    --put which player's turn is it
+    let plChar = playerToChar turn
+    putStrLn ("It is ")
+    putChar(plChar)
+    putStrLn("'s turn:")
+    answer <- randomRIO(1,7); --put our value read into answer
+    if not (answer== 0)
+    then --process turn change and repeat play
+        if isSlotOpen bd answer --check if a slot is open
             then do 
                 if turn == 1
-                then dropInSlot bd answer player--drop it with the player
-                else dropInSlot bd answer opponnent --drop it with the opponent
-                if isWonBy bd turn --if player won with this movement we quit
-                then do
-                    putStrLn "Congrats!!" --program ends
-                else if isFull bd --check if board is full
-                then do
-                    putStrLn "It was a tie!"
-                else--we change the turn
-                    main --remmeber to change this part to change the turn
-            else putStrLn "Slot is full, select a different one." 
-            main --repromtp user
+                then do 
+                    --dropInSlot bd answer player--drop it with the player
+                    if isWonBy bd turn --if player won with this movement we quit
+                    then do
+                        putStrLn "Congrats!!" --program ends
+                        return 0
+                    else if isFull bd --check if board is full
+                    then do
+                        putStrLn "It was a tie!"
+                        return 0
+                    else--we change the turn
+                        playAI' --remmeber to change this part to change the turn
+                else do 
+                    --dropInSlot bd answer opponnent --drop it with the opponent
+                    if isWonBy bd turn --if player won with this movement we quit
+                    then do
+                        putStrLn "Congrats!!" --program ends
+                        return 0
+                    else if isFull bd --check if board is full
+                    then do
+                        putStrLn "It was a tie!"
+                        return 0
+                    else--we change the turn
+                        playAI' --remmeber to change this part to change the turn
+            else do
+                playAI'
+    else return 0--terminate program
+
+     {-|
+    The game can end by a player winning, a board being full or the player quitting
+    We will first check if the slot is open, 
+    if so, we drop the checker, check for a player winning, or a full board
+    and change turn
+    -}
+    where 
+        playAI' = do
+            playAI
+        
+main = do
+   --We will prompt user for either play against "AI" or normally
+    putStrLn "Do you wish to play normally or against AI? Enter 1 or 0 respectively to choose"
+    line <- getLine
+    let parse = reads line :: [(Integer, String)] in
+       if length parse == 0  --empty string so reprompt
+       then main'
+       else let (x, _) = head parse in
+        if x == 0 then playAI else play --depending on choice we execute functioins
+    where 
+        main' = do 
+            putStrLn "Incorrect input"
+            main
